6c0879493df5a8b8feba917d753d595122119867
ROnak Kapadi <53690956+ronakkapadi22@users.noreply.github.com>
2022-10-20 15:07:56 +0530

    develop

---
+++ src/helpers/s3-bucket.js
@@ -0,0 +1,29 @@
+import { S3Client } from '@aws-sdk/client-s3'
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+
+const s3 = new S3Client({
+  secretAccessKey: process.env.SECRET_KEY,
+  accessKeyId: process.env.ACCESS_KEY,
+  region: process.env.BUCKET_REGION,
+})
+
+const  BUCKET_NAME = process.env.BUCKET_NAME || 'learn-nova-s3-bucket'
+
+export const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: BUCKET_NAME,
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.originalname});
+    },
+    key: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+})
+
+// export const uploadFiles = (data) => {
+//   return upload.array(data)
+// }
