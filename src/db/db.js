04f281c3694bd6bed7765ea8309eef1bb36403e2
ROnak Kapadi <ronakkapadi22@gmail.com>
2022-12-06 10:27:55 +0530

    Vercel configuration

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose'
 import { config } from '../config/config.js'
 
-
-const db = mongoose.connect(config.CONNECTION_URL).then(() => {
+const options = {
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10*1000, // 1s timeout
+  };
+const db = mongoose.connect(config.CONNECTION_URL, options).then(() => {
     console.log('connection successfully')
 }).catch((error) => {
     console.log('error', error)
